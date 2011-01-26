f5bfbc1530b4210ea8daba13598283ffc6580d90
Andreas Bolka <a@bolka.at>
2011-01-26 05:21:10 +0100

    Rename connection_timeout parameter to connect_timeout

--- beanstalkc.py
+++ beanstalkc.py
@@ -46,22 +46,22 @@ class SocketError(BeanstalkcException):
 
 class Connection(object):
     def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT, parse_yaml=True,
-                 connection_timeout=DEFAULT_TIMEOUT):
+                 connect_timeout=DEFAULT_TIMEOUT):
         if parse_yaml is True:
             try:
                 parse_yaml = __import__('yaml').load
             except ImportError:
                 logging.error('Failed to load PyYAML, will not parse YAML')
                 parse_yaml = False
+        self._connect_timeout= connect_timeout
         self._parse_yaml = parse_yaml or (lambda x: x)
         self.host = host
         self.port = port
-        self.connection_timeout = connection_timeout
         self.connect()
 
     def connect(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.settimeout(self.connection_timeout)
+        self._socket.settimeout(self._connect_timeout)
         SocketError.wrap(self._socket.connect, (self.host, self.port))
         self._socket.settimeout(None)
         self._socket_file = self._socket.makefile('rb')
