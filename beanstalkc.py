351a955deaf28528305966db75313161c4897626
Andreas Bolka <a@bolka.at>
2011-01-28 02:21:37 +0100

    Rename Markdown files to .mkd

--- beanstalkc.py
+++ beanstalkc.py
@@ -285,7 +285,7 @@ if __name__ == '__main__':
         pid = os.spawnlp(os.P_NOWAIT,
                          'beanstalkd',
                          'beanstalkd', '-l', '127.0.0.1', '-p', '14711')
-        doctest.testfile('TUTORIAL.md', optionflags=doctest.ELLIPSIS)
+        doctest.testfile('TUTORIAL.mkd', optionflags=doctest.ELLIPSIS)
         doctest.testfile('test/no-yaml.doctest', optionflags=doctest.ELLIPSIS)
     finally:
         os.kill(pid, signal.SIGTERM)
