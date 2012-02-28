24ceb3b23a4da9be12cf71dd4c0ad633c29f2ef2
Andreas Bolka <a@bolka.at>
2012-02-28 17:55:01 +0100

    Explicitly mark ellipsis usage in doctests

--- beanstalkc.py
+++ beanstalkc.py
@@ -289,7 +289,7 @@ if __name__ == '__main__':
         pid = os.spawnlp(os.P_NOWAIT,
                          'beanstalkd',
                          'beanstalkd', '-l', '127.0.0.1', '-p', '14711')
-        doctest.testfile('TUTORIAL.mkd', optionflags=doctest.ELLIPSIS)
-        doctest.testfile('test/no-yaml.doctest', optionflags=doctest.ELLIPSIS)
+        doctest.testfile('TUTORIAL.mkd')
+        doctest.testfile('test/no-yaml.doctest')
     finally:
         os.kill(pid, signal.SIGTERM)
