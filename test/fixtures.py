5eb8dd744dce9c94a653dca782873724bd9feb8e
Andreas Bolka <a@bolka.at>
2013-01-08 23:48:44 +0100

    Test with custom-built beanstalkd 1.8 on Travis CI
    Our testsuite tests features only available with beanstalkd 1.8+. To
    make this dependency explicit, we now build beanstalkd from source on
    Travis CI instead of relying on on distro-provided packages.

--- test/fixtures.py
+++ test/fixtures.py
@@ -3,10 +3,11 @@ import os, signal, time
 _BEANSTALKD_PID = None
 
 def setup(module):
+    beanstalkd = os.getenv('BEANSTALKD', 'beanstalkd')
     module._BEANSTALKD_PID = os.spawnlp(
             os.P_NOWAIT,
-            'beanstalkd',
-            'beanstalkd', '-l', '127.0.0.1', '-p', '14711')
+            beanstalkd,
+            beanstalkd, '-l', '127.0.0.1', '-p', '14711')
     time.sleep(0.5) # Give beanstalkd some time to ready.
 
 def teardown(module):
